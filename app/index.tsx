6a9dd6118c9f8a263a111a9704cebf82b60f5e5c
Sheheryaar Baber <shezziwork@gmail.com>
2026-08-31 10:43:36 +0100

    Revert "Show a travel time on place cards without waking every row"
    This reverts commit 6dd08b9188d726dd63aea8e094ed00cdeb0ba498.

--- app/index.tsx
+++ app/index.tsx
@@ -711,7 +711,6 @@ export default function HomeScreen() {
       <PlaceCard
         place={item.place}
         distanceLabel={formatDistance(item.kmFromUser)}
-        distanceKm={item.kmFromUser}
         onPress={openPlace}
       />
     ),
@@ -945,7 +944,6 @@ export default function HomeScreen() {
                   key={item.place.id}
                   place={item.place}
                   distanceLabel={formatDistance(item.kmFromUser)}
-                  distanceKm={item.kmFromUser}
                   onPress={openPlace}
                 />
               ))}
